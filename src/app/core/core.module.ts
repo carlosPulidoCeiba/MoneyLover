a30d7f74155c4e4afa4af12936815666b61eca0b
Carlos Andrés Pulido Ortiz - Ceiba Software <carlos.pulido@ceiba.com.co>
2021-09-23 08:54:53 -0500

    build core-module

--- src/app/core/core.module.ts
+++ src/app/core/core.module.ts
@@ -2,8 +2,6 @@ import { NgModule, ErrorHandler } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { SecurityGuard } from './guard/security.guard';
 import { HTTP_INTERCEPTORS } from '@angular/common/http';
-import { TokenInterceptor } from './interceptor/token-interceptor';
-import { AuthInterceptor } from './interceptor/auth-interceptor';
 import { ToolbarComponent } from './components/toolbar/toolbar.component';
 import { NavbarComponent } from './components/navbar/navbar.component';
 import { HttpService } from './services/http.service';
@@ -20,8 +18,6 @@ import { RouterModule } from '@angular/router';
   providers: [
     HttpService,
     SecurityGuard,
-    { provide: HTTP_INTERCEPTORS, useClass: TokenInterceptor, multi: true },
-    { provide: HTTP_INTERCEPTORS, useClass: AuthInterceptor, multi: true },
     { provide: ErrorHandler, useClass: ManejadorError }
   ]
 })
